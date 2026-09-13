683bac96cbbe83e1f35a2ae148ea75b2aff7cae5
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-13 21:59:40 +0200

    Fix synchronous UX bundle loader

--- jgw-library-nav.js
+++ jgw-library-nav.js
@@ -1,9 +1,9 @@
 (function(){
 "use strict";
-/* Synchronous UX bundle loader. This file stays as the stable entrypoint referenced by index.html. */
+/* Stable entrypoint referenced by index.html. The split files load synchronously before the next app script. */
 if(window.__jgwUxBundleLoader)return;window.__jgwUxBundleLoader=true;
 var base=(document.currentScript&&document.currentScript.src)||location.href;
 var root=base.slice(0,base.lastIndexOf("/")+1);
-function tag(file,version){return '<script src="'+root+file+'?v='+version+'"><\\/script>'}
-document.write(tag("jgw-ux-shell.js","20260913-1")+tag("jgw-library-v4.js","20260913-1"));
+function tag(file,version){return '<script src="'+root+file+'?v='+version+'"></'+'script>'}
+document.write(tag("jgw-ux-shell.js","20260913-2")+tag("jgw-library-v4.js","20260913-2"));
 })();
