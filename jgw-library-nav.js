2fe2b17b783fdf2c1b8ebf17173aedc8dcd768fa
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-14 06:07:30 +0200

    Refresh split module versions after cleanup

--- jgw-library-nav.js
+++ jgw-library-nav.js
@@ -5,5 +5,5 @@ if(window.__jgwUxBundleLoader)return;window.__jgwUxBundleLoader=true;
 var base=(document.currentScript&&document.currentScript.src)||location.href;
 var root=base.slice(0,base.lastIndexOf("/")+1);
 function tag(file,version){return '<script src="'+root+file+'?v='+version+'"></'+'script>'}
-document.write(tag("jgw-ux-shell.js","20260913-3")+tag("jgw-library-v4.js","20260913-3")+tag("jgw-ux-patch.js","20260913-3")+tag("jgw-photo-storage-v2.js","20260913-2"));
+document.write(tag("jgw-ux-shell.js","20260914-1")+tag("jgw-library-v4.js","20260914-1")+tag("jgw-ux-patch.js","20260914-1")+tag("jgw-photo-storage-v2.js","20260914-2"));
 })();
