86ef8a2782f158920dbeb9d93de9442f5c0971e2
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-20 13:05:54 +0200

    Vokabeltrainer iPhone Beta veröffentlichen

---
+++ vokabeltrainer/js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(async function bootstrap(){
+  state = await loadState();
+  if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg=>reg.update().catch(()=>{}))
+      .catch(console.warn);
+  }
+  bind();
+  renderAll();
+})();
