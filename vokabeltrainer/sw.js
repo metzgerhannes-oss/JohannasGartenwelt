7c7d8d2366f49d9d8326bbd73787f9126452ebce
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-20 17:02:40 +0200

    Vokabeltrainer v0.9.14: Cache-Busting

--- vokabeltrainer/sw.js
+++ vokabeltrainer/sw.js
@@ -1,5 +1,5 @@
-const CACHE='vokabeltrainer-v0.9.13-sense-final';
-const ASSETS=['./','./index.html','./css/app.css?v=0.9.13','./js/core.js?v=0.9.13','./js/storage.js?v=0.9.13','./js/model.js?v=0.9.13','./js/learning.js?v=0.9.13','./js/translation.js?v=0.9.13','./js/io.js?v=0.9.13','./js/ui.js?v=0.9.13','./js/app.js?v=0.9.13','./manifest.webmanifest','./assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
+const CACHE='vokabeltrainer-v0.9.14-sense-final';
+const ASSETS=['./','./index.html','./css/app.css?v=0.9.14','./js/core.js?v=0.9.14','./js/storage.js?v=0.9.14','./js/model.js?v=0.9.14','./js/learning.js?v=0.9.14','./js/translation.js?v=0.9.14','./js/io.js?v=0.9.14','./js/ui.js?v=0.9.14','./js/app.js?v=0.9.14','./manifest.webmanifest','./assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil((async()=>{
   const cache=await caches.open(CACHE);
   const requests=ASSETS.map(path=>new Request(new URL(path,self.location).href,{cache:'reload'}));
