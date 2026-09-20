2fc6aa90f91862d6193575789773171c0d240a39
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-20 16:32:00 +0200

    Vokabeltrainer v0.9.12: getrennte Bedeutungen

--- vokabeltrainer/js/app.js
+++ vokabeltrainer/js/app.js
@@ -3,7 +3,7 @@
 (async function bootstrap(){
   state = await loadState();
   if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js?v=0.9.11')
+    navigator.serviceWorker.register('./sw.js?v=0.9.12')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
